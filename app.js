d5a4c2063987d36f9dd0411c3fc54983db613b5a
RiccardoS <rspinellir13@gmail.com>
2025-01-16 20:02:03 +0100

    Ejercicio Terminado

--- app.js
+++ app.js
@@ -1,51 +1,46 @@
 const express = require('express');
 const app = express();
 
-app.use(express.urlencoded({ extended: true })); //WTF
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 let usuarios = [
-  { id: 1, nombre: 'Ryu' },
-  { id: 2, nombre: 'Ken' },
-  { id: 3, nombre: 'Blanca' },
+    { id: 1, nombre: 'Ryu', edad: 32, lugarProcedencia: 'Japón' },
+    { id: 2, nombre: 'Chun-Li', edad: 29, lugarProcedencia: 'China' },
+    { id: 3, nombre: 'Guile', edad: 35, lugarProcedencia: 'Estados Unidos' },
+    { id: 4, nombre: 'Dhalsim', edad: 45, lugarProcedencia: 'India' },
+    { id: 5, nombre: 'Blanka', edad: 32, lugarProcedencia: 'Brasil' },
 ];
 
-// READ
-app.get('/', (req, res) => {
-  res.send(`
-    <h1>Street Fighter</h1>
-    <ul>
-      ${usuarios
-        .map(
-          (usuarios) =>
-            `<li>ID: ${usuarios.id} || Nombre: ${usuarios.nombre}</li>`
-        )
-        .join('')}
-    </ul>
-    <form action="/usuarios" method="post">
-        <label for="nombre">NOMBRE</label>
-        <input type="text" id="nombre" name="nombre" required>
-        <button type="submit">Agregar Usuario</button>
-    </form>
-    <a href="/usuarios">USUARIOS JJHONSON</a>
-  `);
-});
-
 app.get('/usuarios', (req, res) => {
-  res.json(usuarios);
+    res.json(usuarios);
 });
 
-app.post('/usuarios', (req, res) => {
-  //    WTF
-  const nuevoUsuario = {
-    id: usuarios.length + 1,
-    nombre: req.body.nombre,
-  };
-  usuarios.push(nuevoUsuario);
-  res.redirect('/');
+app.post("/usuarios", (req, res) => {
+
+    const nuevoUsuario = {
+        id: usuarios.length +1,
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        lugarProcedencia: req.body.lugarProcedencia,
+    }
+
+    usuarios.push(nuevoUsuario);
+    res.json(nuevoUsuario)
+});
+
+app.get('/usuarios/:nombre', (req, res) => {
+    const nombre = req.params.nombre;
+    const usuario = usuarios.find(u => u.nombre.toLowerCase() === nombre.toLowerCase());
+
+    if (usuario) {
+        res.json(usuario);
+    } else {
+        res.status(404).json({ mensaje: 'Usuario no encontrado' });
+    }
 });
 
 const PORT = 3000;
-
-app.listen(3000, () => {
-  console.log(`Express listening in http://localhost:${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
